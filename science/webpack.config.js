7993015515490d753a4c64678764f47917ed7d68
Patrick O'Connor <ocopatr@amazon.com>
2021-04-30 16:30:55 +1000

    correcting references

--- science/webpack.config.js
+++ science/webpack.config.js
@@ -1,10 +1,10 @@
-const { merge }  = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
-    orgName: "University",
-    projectName: "History",
+    orgName: "asset",
+    projectName: "manifest",
     webpackConfigEnv,
     argv,
   });
